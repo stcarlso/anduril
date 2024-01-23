ba1c88776b12c6447de5909bc425a4c17c14689a
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2024-01-23 00:03:57 -0600

    fix two separate bugs around RGB aux channels
    First off, green was not defined properly and was instead defined the
    same as a main channel (leading to the behaviour described in the issue
    at https://github.com/ToyKeeper/anduril/pull/33#issuecomment-1891341281
    
    Second, fix the actual bug causing aux to not work for blinky modes.
    Specifically, set_level needs to be patched so that instead of (or as
    well as, doesn't really matter) calling set_level_zero(), it will also
    check for if the active channel is an RGB channel and if so them clear
    RGB state.

--- fsm/ramping.c
+++ fsm/ramping.c
@@ -49,11 +49,13 @@ inline void set_level_aux_leds(uint8_t level) {
 // TODO: maybe move this stuff into FSM
 #include "anduril/aux-leds.h"  // for rgb_led_voltage_readout()
 inline void set_level_aux_rgb_leds(uint8_t level) {
-    if ((! go_to_standby)
-       #ifdef USE_CHANNEL_USES_AUX
-       && (!channel_uses_aux(channel_mode))
-       #endif
-       ){
+    if (
+        #ifdef USE_CHANNEL_USES_AUX
+        (!channel_uses_aux(channel_mode))
+        #else
+        (! go_to_standby)
+        #endif
+        ){
         #ifdef USE_AUX_THRESHOLD_CONFIG
         if (level > cfg.button_led_low_ramp_level) {
             rgb_led_voltage_readout(level > cfg.button_led_high_ramp_level);
@@ -109,11 +111,24 @@ void set_level(uint8_t level) {
     #endif
 
     #ifdef USE_AUX_RGB_LEDS_WHILE_ON
+        #ifdef USE_CHANNEL_USES_AUX
+        if (!channel_uses_aux(channel_mode)) {
+            set_level_aux_rgb_leds(level);
+        }
+        #else
+        set_level_aux_rgb_leds(level);
+        #endif
+
     set_level_aux_rgb_leds(level);
     #endif
 
     if (0 == level) {
         set_level_zero();
+        #ifdef USE_CHANNEL_USES_AUX
+            if (channel_uses_aux(channel_mode)){
+                rgb_led_set(0);
+            }
+        #endif
     } else {
         // call the relevant hardware-specific set_level_*()
         SetLevelFuncPtr set_level_func = channels[channel_mode].set_level;
