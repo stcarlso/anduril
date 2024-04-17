0d363dbf42db4ee35d1f435992976f73b1b1af66
Stephen Carlson <stcarlso@gmail.com>
2024-04-17 00:22:56 -0700

    Add noods-a Adafruit Noods Driver rev A hwdef

---
+++ hw/stcarlso/anduril.h
@@ -0,0 +1,31 @@
+// stcarlso's config options for Anduril
+// Copyright (C) 2021-2023 Selene ToyKeeper
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+#ifdef RGB_LED_OFF_DEFAULT
+#undef RGB_LED_OFF_DEFAULT
+#endif
+#define RGB_LED_OFF_DEFAULT auxrgb_cfg_byte(aux_low_e, aux_rgb_voltage_e)
+#define RGB_LED_LOCKOUT_DEFAULT auxrgb_cfg_byte(aux_low_e, aux_rgb_red_e)
+
+// Off mode: low (1)
+// Lockout: low (1)
+#ifdef AUX1_DEFAULT_MODE
+#undef AUX1_DEFAULT_MODE
+#endif
+#define AUX1_DEFAULT_MODE ((1<<2) + 1)
+
+// Half a second per color in rainbow mode
+#define RGB_RAINBOW_SPEED 0x03
+
+// Allow 3C (or 6C) in Simple UI (toggle smooth or stepped ramping)
+#define USE_SIMPLE_UI_RAMPING_TOGGLE
+
+// Double click while on goes to full-power turbo, not ramp ceiling
+#define DEFAULT_2C_STYLE 1
+
+#define USE_SOFT_FACTORY_RESET
+
+// No post-off voltage display by default
+#define DEFAULT_POST_OFF_VOLTAGE_SECONDS 0
