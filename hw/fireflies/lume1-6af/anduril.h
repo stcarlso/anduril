bb0785140536973e2ed9f4dd3c3417b4eb36ebf8
Stephen Carlson <stcarlso@gmail.com>
2024-05-13 22:40:18 -0700

    Update default configuration of fireflies-lume1-6af for NOV-MU v2

--- hw/fireflies/lume1-6af/anduril.h
+++ hw/fireflies/lume1-6af/anduril.h
@@ -16,5 +16,50 @@
 #undef THERM_RESPONSE_MAGNITUDE
 #define THERM_RESPONSE_MAGNITUDE 64
 
+// Disable default loneoceans values
+#undef DEFAULT_BLINK_CHANNEL
+#undef DEFAULT_LEVEL
 #undef RAMP_SMOOTH_FLOOR
-#define RAMP_SMOOTH_FLOOR 9
+#undef RAMP_SMOOTH_CEIL
+#undef RAMP_DISCRETE_FLOOR
+#undef RAMP_DISCRETE_CEIL
+#undef RAMP_DISCRETE_STEPS
+#undef SIMPLE_UI_FLOOR
+#undef SIMPLE_UI_CEIL
+#undef SIMPLE_UI_STEPS
+#undef RGB_LED_OFF_DEFAULT
+#undef RGB_LED_LOCKOUT_DEFAULT
+#undef BLINK_BRIGHTNESS
+
+// blink numbers on the aux LEDs by default (but allow user to change it)
+#define DEFAULT_BLINK_CHANNEL  CM_AUXWHT
+
+#define DEFAULT_LEVEL      41
+// Default lowest is 9
+#define RAMP_SMOOTH_FLOOR  5
+#define RAMP_SMOOTH_CEIL   149
+// 5 [41] 77 113 149
+#define RAMP_DISCRETE_FLOOR RAMP_SMOOTH_FLOOR
+#define RAMP_DISCRETE_CEIL  RAMP_SMOOTH_CEIL
+#define RAMP_DISCRETE_STEPS 5
+
+// 5 [41] 77 113 149
+#define SIMPLE_UI_FLOOR  RAMP_DISCRETE_FLOOR
+#define SIMPLE_UI_CEIL   RAMP_DISCRETE_CEIL
+#define SIMPLE_UI_STEPS  5
+
+#define DEFAULT_MANUAL_MEMORY 41
+#define DEFAULT_MANUAL_MEMORY_TIMER 5
+
+#define BLINK_BRIGHTNESS 41
+
+// low, battery
+#define RGB_LED_OFF_DEFAULT 0x19
+// low, red
+#define RGB_LED_LOCKOUT_DEFAULT 0x10
+
+// Allow 3C (or 6C) in Simple UI (toggle smooth or stepped ramping)
+#define USE_SIMPLE_UI_RAMPING_TOGGLE
+
+// No post-off voltage display by default
+#define DEFAULT_POST_OFF_VOLTAGE_SECONDS 0
