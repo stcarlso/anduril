f4e19416cf78b5b30d4ef3020c0ec09a2eb2c83a
Stephen Carlson <stcarlso@gmail.com>
2024-06-21 00:43:02 -0700

    hw: hank: Enable ultra-low mode config
    Add the configuration option for ultra-low mode to some Hank linear
    driven single and dual channel lights that support it.
    
    # Conflicts:
    #	hw/hank/noctigon-kr4/anduril.h

--- hw/hank/noctigon-kr4/nofet/hwdef.c
+++ hw/hank/noctigon-kr4/nofet/hwdef.c
@@ -30,13 +30,33 @@ void set_level_zero() {
 
 // single set of LEDs with linear power channel
 void set_level_main(uint8_t level) {
+#ifdef USE_ULTRA_LOW_MODE
+    level_1_mode_e mode = cfg.ultra_low_mode;
+
+#ifdef USE_AUX_RGB_LEDS
+    if (level == 0 && mode == level_1_redaux) {
+        set_level_auxred(1);
+        set_level_zero();
+        return;
+    }
+#endif
+#endif
+
     CH1_ENABLE_PORT |= (1 << CH1_ENABLE_PIN);  // enable opamp
 
     PWM_DATATYPE ch1_pwm = PWM_GET(pwm1_levels, level);
     // pulse frequency modulation, a.k.a. dynamic PWM
     uint16_t top = PWM_GET16(pwm_tops, level);
 
-    CH1_PWM = ch1_pwm;
+    if (0) { }
+#ifdef USE_ULTRA_LOW_MODE
+    else if (level == 0 && mode >= level_1_s0) {
+        CH1_PWM = 0;
+    }
+#endif
+    else {
+        CH1_PWM = ch1_pwm;
+    }
     CH2_PWM = 0;
     // wait to sync the counter and avoid flashes
     while(actual_level && (PWM_CNT > (top - 32))) {}
@@ -50,6 +70,12 @@ bool gradual_tick_main(uint8_t gt) {
     PWM_DATATYPE pwm1 = PWM_GET(pwm1_levels, gt);
 
     GRADUAL_ADJUST_SIMPLE(pwm1, CH1_PWM);
+
+#ifdef USE_ULTRA_LOW_MODE
+    if (gt == 0 && cfg.ultra_low_mode >= level_1_s0) {
+        pwm1 = 0U;
+    }
+#endif
 
     if (   (pwm1 == CH1_PWM)
        ) {
