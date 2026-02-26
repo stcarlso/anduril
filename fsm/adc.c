7cbc823e90041325b9f5bea4c8eafbac5d3d373b
loneoceans <loneoceans@gmail.com>
2026-02-26 11:00:54 -0800

    fix lume1 ADC handling broken in previous commit

--- fsm/adc.c
+++ fsm/adc.c
@@ -349,16 +349,21 @@ static void ADC_temperature_handler() {
             int16_t howmuch = (offset + offset - 3) * THERM_RESPONSE_MAGNITUDE / 128;
             if (howmuch < 1) howmuch = 1;
             warning_threshold = THERM_NEXT_WARNING_THRESHOLD - (uint8_t)howmuch;
-            
+
             // if TURBO_TEMP_EXTRA is defined, allow to run hotter in turbo until limit is hit
             #ifdef TURBO_TEMP_EXTRA
-            if ((temperature < (TH_CEIL + TURBO_TEMP_EXTRA)) && (actual_level >= RAMP_SIZE) ){
+            // clamp to be maximum of 71C
+            int8_t th_max = TH_CEIL + TURBO_TEMP_EXTRA;
+            if (th_max > 71){th_max = 71;}
+
+            if ((temperature < th_max) && (actual_level >= RAMP_SIZE) ){
                 ; // take no action
             } 
             else{
                 emit(EV_temperature_high, howmuch);
             }
             #else
+
             // send a warning
             emit(EV_temperature_high, howmuch);
             #endif
